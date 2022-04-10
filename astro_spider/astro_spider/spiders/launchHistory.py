9dd6eef0ff012c34dc7271240b917520780dd2c1
shiroininjaTech <47760502+shiroininjaTech@users.noreply.github.com>
2022-04-10 10:11:07 -0400

    merged from Unstable repo 4/22
    Scrapes Virgin Orbit launches for tallying now.

--- astro_spider/astro_spider/spiders/launchHistory.py
+++ astro_spider/astro_spider/spiders/launchHistory.py
@@ -6,7 +6,7 @@ from scrapy.http.request import Request
 class LaunchhistorySpider(scrapy.Spider):
     name = 'launchHistory'
     allowed_domains = ['rocketlaunch.live']
-    start_urls = ['https://www.rocketlaunch.live/?pastOnly=1&page=1', 'https://www.rocketlaunch.live/?pastOnly=1&page=2','https://www.rocketlaunch.live/?pastOnly=1&page=3', 'https://www.rocketlaunch.live/?pastOnly=1&page=4', 'https://www.rocketlaunch.live/?pastOnly=1&page=5', 'https://www.rocketlaunch.live/?pastOnly=1&page=6', 'https://www.rocketlaunch.live/?pastOnly=1&page=7', 'https://www.rocketlaunch.live/?pastOnly=1&page=8', 'https://www.rocketlaunch.live/?pastOnly=1&page=9']
+    start_urls = ['https://www.rocketlaunch.live/?pastOnly=1&page=1', 'https://www.rocketlaunch.live/?pastOnly=1&page=2','https://www.rocketlaunch.live/?pastOnly=1&page=3', 'https://www.rocketlaunch.live/?pastOnly=1&page=4', 'https://www.rocketlaunch.live/?pastOnly=1&page=5', 'https://www.rocketlaunch.live/?pastOnly=1&page=6', 'https://www.rocketlaunch.live/?pastOnly=1&page=7', 'https://www.rocketlaunch.live/?pastOnly=1&page=8', 'https://www.rocketlaunch.live/?pastOnly=1&page=9', 'https:www.rocketlaunch.live/?pastOnly=1&page=10']
 
     custom_settings = {'LOG_ENABLED': False,
     }
@@ -26,7 +26,7 @@ class LaunchhistorySpider(scrapy.Spider):
             'russiaDate' : response.xpath(".//div[contains(@class, 'company-roscosmos')]/div[@class='launch_info_left_container large-2 medium-2 small-3 columns']/div[@data-sortdate]/@title").extract(),
             'northropDate' : response.xpath(".//div[contains(@class, 'company-northrop-grumman')]/div[@class='launch_info_left_container large-2 medium-2 small-3 columns']/div[@data-sortdate]/@title").extract(),
             'blueDate' : response.xpath(".//div[contains(@class, 'company-blue-origin')]/div[@class='launch_info_left_container large-2 medium-2 small-3 columns']/div[@data-sortdate]/@title").extract(),
-            'ilsDate' : response.xpath(".//div[contains(@class, 'company-international-launch-services-ils')]/div[@class='launch_info_left_container large-2 medium-2 small-3 columns']/div[@data-sortdate]/@title").extract(),
+            'virginDate' : response.xpath(".//div[contains(@class, 'company-virgin-orbit')]/div[@class='launch_info_left_container large-2 medium-2 small-3 columns']/div[@data-sortdate]/@title").extract(),
             'rusMil' : response.xpath(".//div[contains(@class, 'company-russian-military')]/div[@class='launch_info_left_container large-2 medium-2 small-3 columns']/div[@data-sortdate]/@title").extract(),
             'expace' : response.xpath(".//div[contains(@class, 'company-expace-china')]/div[@class='launch_info_left_container large-2 medium-2 small-3 columns']/div[@data-sortdate]/@title").extract(),
 
